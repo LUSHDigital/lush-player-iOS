74da0a26dd6e95bc760a455eb63b8c39ed5d296a
Simon Mitchell <simon@3sidedcube.com>
2017-01-30 09:41:17 +0000

    Fixes of UI of Lush Channels view

---
+++ Lush Player/ChannelProgrammeCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProgrammeCollectionViewCell.swift
+//  Lush Player
+//
+//  Created by Simon Mitchell on 02/12/2016.
+//  Copyright © 2016 ThreeSidedCube. All rights reserved.
+//
+
+import UIKit
+
+class ChannelProgrammeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+        
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        
+        imageView.adjustsImageWhenAncestorFocused = true
+        imageView.clipsToBounds = false
+    }
+}
