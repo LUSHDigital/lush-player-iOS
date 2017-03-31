e6cfb3448f2dffd4586acd7c857d450998d1e50b
Joel Trew <joel@3sidedcube.com>
2017-03-31 13:53:20 +0100

    Design fixes

--- Lush Player iOS/OffAirViewController.swift
+++ Lush Player iOS/OffAirViewController.swift
@@ -21,15 +21,11 @@ class OffAirViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func pressedExploreChannel(_ sender: SpacedCharacterButton) {
+        
+        self.tabBarController?.selectedIndex = 2
+        
     }
-    */
-
+    
 }
