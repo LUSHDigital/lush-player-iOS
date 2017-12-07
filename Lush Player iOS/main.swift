ee3f706e58e2dd4362234bfbaa75f9c166114a16
Joel Trew <joel.trew@lush.co.uk>
2017-12-07 11:01:59 +0000

    Adds apache software license at head of each file

--- Lush Player iOS/main.swift
+++ Lush Player iOS/main.swift
@@ -2,13 +2,30 @@
 //  main.swift
 //  Lush Player
 //
-//  Created by Joel Trew on 04/04/2017.
-//  Copyright © 2017 ThreeSidedCube. All rights reserved.
-//
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
 
 import Foundation
 import UIKit
 
+// Hack to stop generic view controllers from crashing when used with objective c storyboards
+// TODO: - Find better solution
 print(HomeCollectionViewController.self, ChannelListingViewController.self, SearchResultsViewController.self, EventViewController.self, EventListingViewController.self, TagListViewController.self)
 
 // The following is required because there's an impedence mismatch between
